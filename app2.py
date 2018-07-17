099864a3e9f5290c21ea9282ac52688a36472d40
Brandon Leser <brandon.leser@gmail.com>
2018-07-17 13:39:25 -0700

    made the program use only get requests

---
+++ app2.py
@@ -0,0 +1,56 @@
+"""All the imports, everything from flask for the web/API part
+requests to pull webpages and Beautiful Soup to parse/scrap them"""
+
+from flask import Flask, jsonify, request, render_template
+import requests
+from bs4 import BeautifulSoup
+
+
+app = Flask("__name__")           #Make an instance of the Flask class
+country_list = []                 #Create global variable to hold all the data pulled from the web
+
+
+def get_data():
+    url = 'https://www.worldstandards.eu/cars/list-of-left-driving-countries/'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    data = soup.find_all('td')
+    return data
+
+
+def load_data(data):
+    i = 0
+    while i < len(data):
+        new_entry = {
+            'Country': data[i].text,
+            'Side of Road': data[i+1].text
+        }
+        country_list.append(new_entry)
+        i += 2
+    return country_list
+
+
+def data_list(country):
+    for countries in country_list:
+        if country.lower() in countries['Country'].lower():
+            x = countries['Country']
+            y = countries['Side of Road']
+            return "The Country {}, {}".format(x, y)
+    return "I can't seem to find that Country :(   "
+
+
+@app.route('/')
+def index():
+    return "Type in /'countryname'  placing the desired\n country name in your browser to get results"
+
+@app.route('/<string:name>')
+def results(name):
+    return data_list(name)
+
+@app.route('/complete_dictionary')
+def get_country():
+    return jsonify({'country_list': country_list})
+
+
+load_data(get_data())
+app.run(port=5000)
