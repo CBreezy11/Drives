7f3f52953afa19e79485798e1615e447fe8e2990
Brandon Leser <brandon.leser@gmail.com>
2018-07-16 23:53:44 -0700

    everything works

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, jsonify, request
+"""All the imports, everything from flask for the web/API part
+requests to pull webpages and Beautiful Soup to parse/scrap them"""
+
+from flask import Flask, jsonify, request, render_template
 import requests
 from bs4 import BeautifulSoup
 
 
-app = Flask("__name__")
+app = Flask("__name__")           #Make an instance of the Flask class
+country_list = []                 #Create global variable to hold all the data pulled from the web
 
-country_list = []
+"""Define method to connect to webpage and return
+the html parts with the desired data"""
 
 def get_data():
     url = 'https://www.worldstandards.eu/cars/list-of-left-driving-countries/'
@@ -13,6 +18,9 @@ def get_data():
     soup = BeautifulSoup(page.text, 'html.parser')
     data = soup.find_all('td')
     return data
+
+"""define method to parse the data into
+something we can add in to our global list"""
 
 def load_data(data):
     i = 0
@@ -25,31 +33,45 @@ def load_data(data):
         i += 2
     return country_list
 
-def format_user(name):
-    name = name.lower()
-    name = name.capitalize()
-    return name
+"""Define a module to take the user input
+and search through the list for a match. This
+needs to account for using of capitilization in
+input and data. If match is found format a string that gets passed to the 
+html file.  If not found format a string saying so"""
 
-"""@app.route('/', methods=['POST', 'GET'])
-def home():
-    pass"""
-
-    
-
-@app.route('/<country>')
 def data_list(country):
     for countries in country_list:
         if country.lower() in countries['Country'].lower():
             x = countries['Country']
             y = countries['Side of Road']
             return "The Country {}, {}".format(x, y)
-    return "I can't seem to find that Country :("
+    return "I can't seem to find that Country :(   "
 
+"""API will run on / with a form asking for 
+input of a country.  Then sends to data_list to handle
+the work and data_list returns the output back here
+where the templates get rendered"""
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    greeting = "Hello World"
+    if request.method == 'POST':
+        country = request.form['country']
+        greeting = data_list(country)
+        return render_template('index.html', greeting = greeting)
+    else:
+        return render_template('hello_form.html')
+
+"""Create a path where the entire data_list
+can be seen, just for funsies"""
+          
 @app.route('/complete_dictionary')
 def get_country():
     return jsonify({'country_list': country_list})
 
-
+"""Call load_data passing get_data to
+create our data and start the API on
+port 5000"""
 
 load_data(get_data())
 app.run(port=5000)
